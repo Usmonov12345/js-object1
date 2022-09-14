3aed8a9d3c1fb463f02f2435e5a4fe75ad4a95b2
Usmonov12345 <toxirusmonov04@gmail.com>
2022-09-14 14:23:04 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,71 @@
+// var a = -5;
+// if (a){
+//     console.log("done");
+// }
+// else{
+//     console.log("fail");
+// }
+
+// var a = 5;
+// if (a){
+//     console.log("done");
+// }
+// else{
+//     console.log("fail");
+// }
+
+// var a = 6, b = 5;
+// if(a > 0 && b >0){
+//     console.log("done");
+// }else{
+// console.log("file");
+// }
+
+// var a = 6, b = 5, c = 4;
+
+// if(a > b && b > c){
+//     console.log("done");
+// }
+// else{
+//     console.log("file");
+// }
+
+// var a = 3, b = 15, C = 40;
+// if (a < b &&  b > a ){
+// console. log("True");
+// }else {
+// console. log("False");
+// }
+
+// var a = 3, b = 5;
+// if (!a % 2 == 0 && !b % 2 == 0){
+// console. log ("Toq");
+// }else {
+// console. log( "Har ikkisi toq emas");
+// }
+
+
+// var a = 1, b = -0.1, C = -0.11 , d = -5;
+// if (a > 0 && b > 0 && C > O && d > 0){
+// console. log( "True");
+// }else {
+// console. log( "False");
+// }
+
+// var a = 1, b = -0.1, C = -0.11 , d = -5;
+// if (a > 0 && b > 0 && C > O && d > 0){
+// console. log( "True");
+// }else {
+// console. log( "False");
+// }
+
+// var kunlar
+// ["",
+// "Dushanba", "Seshanba",
+// " Chorshanba",
+// "Payshanba", "Juma",
+// "Shanba",
+// "Yakshanba"];
+// console. log('kunlar' [2]);
+
+
